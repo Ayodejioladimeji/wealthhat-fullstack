cdf909329e9ef4dbe7e7bee3912acc9f7fd3b41e
Ayodejioladimeji <layobright11@gmail.com>
2022-01-17 07:03:13 -0800

    completed the user schema

--- models/userModel.js
+++ models/userModel.js
@@ -12,6 +12,60 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    title: {
+      type: String,
+      default: '',
+    },
+    firstname: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    gender: {
+      type: String,
+      default: '',
+    },
+    nationality: {
+      type: String,
+      default: '',
+    },
+    dayofbirth: {
+      type: String,
+      default: '',
+    },
+    monthofbirth: {
+      type: String,
+      default: '',
+    },
+    yearofbirth: {
+      type: String,
+      default: '',
+    },
+    phone: {
+      type: Number,
+      default: '',
+    },
+    address: {
+      type: String,
+      default: '',
+    },
+    bvn: {
+      type: Number,
+      default: '',
+    },
+    accountNumber: {
+      type: Number,
+      default: '',
+    },
+    bank: {
+      type: String,
+      default: '',
+    },
     role: {
       type: Number,
       default: 0, // 0=user, 1=admin
