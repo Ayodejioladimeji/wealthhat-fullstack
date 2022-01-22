ef046113654c83bb4c4b88aa9b519b105eb2e122
Ayodejioladimeji <layobright11@gmail.com>
2022-01-22 00:49:41 -0800

    added path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 // Initialize express
 const app = express();
